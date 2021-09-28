b493a84e08abc28d6a39e02cb7416bb276b78a94
Wilson Wei Chen Huang <48188884+willyyhuang@users.noreply.github.com>
2021-09-27 20:31:54 -0400

    init step 6

--- index.py
+++ index.py
@@ -1,10 +1,12 @@
-import numpy as np
 import matplotlib.pyplot as plt
 from sklearn import datasets
 from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.model_selection import train_test_split
+from sklearn.naive_bayes import MultinomialNB
 
+classInstances = ['business', 'entertainment', 'politics', 'sport', 'tech']
 # step 2
-plt.plot(['business', 'entertainment', 'politics', 'sport', 'tech'], [501, 386, 417, 511, 401])
+plt.bar(classInstances, [501, 386, 417, 511, 401])
 plt.savefig('BBC-distribution.pdf')
 
 # step 3, 4
@@ -13,5 +15,9 @@ vectorizer = CountVectorizer()
 term_document_matrix = vectorizer.fit_transform(corpus.data)
 
 # step 5
+training_set, testing_set = train_test_split(corpus.data, test_size=0.2, random_state=None)
 
-
+# step 6
+clf = MultinomialNB()
+# clf.fit(training_set, classInstances)
+# print(clf.predict(testing_set))
