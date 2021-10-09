7b8df75e6eb019c80ca1e977f741f5764f6defd5
Wilson Wei Chen Huang <48188884+willyyhuang@users.noreply.github.com>
2021-10-09 16:45:16 -0400

    update

--- index.py
+++ index.py
@@ -3,6 +3,7 @@ from sklearn import datasets
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.model_selection import train_test_split
 from sklearn.naive_bayes import MultinomialNB
+import numpy as np
 
 classInstances = ['business', 'entertainment', 'politics', 'sport', 'tech']
 # step 2
@@ -10,14 +11,17 @@ plt.bar(classInstances, [501, 386, 417, 511, 401])
 plt.savefig('BBC-distribution.pdf')
 
 # step 3, 4
-corpus = datasets.load_files('./data/BBC-20210914T194535Z-001/BBC', encoding='latin1')
+corpus = datasets.load_files('./BBC', encoding='latin1')
 vectorizer = CountVectorizer()
-term_document_matrix = vectorizer.fit_transform(corpus.data)
+X = vectorizer.fit_transform(corpus.data)
 
 # step 5
-training_set, testing_set = train_test_split(corpus.data, test_size=0.2, random_state=None)
+X_train, X_test, y_train, y_test = train_test_split(X,corpus.target, test_size=0.2,random_state=None)
 
 # step 6
 clf = MultinomialNB()
-# clf.fit(training_set, classInstances)
-# print(clf.predict(testing_set))
+clf.fit(X_train, y_train)
+predicted = clf.predict(X_test)
+
+# step 7
+f = open('bbc-performance.txt', 'w')
