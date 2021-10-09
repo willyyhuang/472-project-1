4be3dd391c5ea2c62595b4d6ad2089acc33a394c
IanjhPhillips <ianjhphillips@gmail.com>
2021-10-09 17:55:59 -0400

    7d and 7f

--- index.py
+++ index.py
@@ -1,27 +1,67 @@
-import matplotlib.pyplot as plt
-from sklearn import datasets
+import numpy as np
+import os
+from sklearn.datasets import load_files
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.model_selection import train_test_split
 from sklearn.naive_bayes import MultinomialNB
-import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.metrics import confusion_matrix
+from sklearn.metrics import classification_report
+from sklearn.metrics import accuracy_score
+from sklearn.metrics import f1_score
+from pandas import *
 
-classInstances = ['business', 'entertainment', 'politics', 'sport', 'tech']
-# step 2
-plt.bar(classInstances, [501, 386, 417, 511, 401])
-plt.savefig('BBC-distribution.pdf')
+datadir="BBC"
+categories=["tech", "entertainment", "politics", "business", "sport"]
+txt_array=[];
+all_array=[];
+for category in categories:  # for each class
+    path = os.path.join(datadir,category)  # create path to class
+    for txt in os.listdir(path):  # iterate over each text file per class
+        txt_array.append(txt)
+    all_array.append(len(txt_array))
+    txt_array=[]
+fig = plt.figure()
+plt.bar(categories,all_array)
+fig.savefig('BBC-distribution.pdf', dpi=fig.dpi)
 
-# step 3, 4
-corpus = datasets.load_files('./BBC', encoding='latin1')
+corpus=load_files(container_path=datadir,encoding="latin1")
+
 vectorizer = CountVectorizer()
 X = vectorizer.fit_transform(corpus.data)
 
-# step 5
 X_train, X_test, y_train, y_test = train_test_split(X,corpus.target, test_size=0.2,random_state=None)
 
-# step 6
 clf = MultinomialNB()
 clf.fit(X_train, y_train)
 predicted = clf.predict(X_test)
 
-# step 7
 f = open('bbc-performance.txt', 'w')
+f.write('---------------------------------------------------------------------------------\n')
+f.write('0 - business category\n')
+f.write('1 - entertainment category\n')
+f.write('2 - politics category\n')
+f.write('3 - sport category\n')
+f.write('4 - tech category\n')
+f.write('---------------------------------------------------------------------------------\n')
+f.write('b) Confusion matrix\n')
+matrix = confusion_matrix(y_test, predicted)
+f.write(DataFrame(matrix).to_string() + '\n')
+f.write('---------------------------------------------------------------------------------\n')
+f.write('c) Classification Report\n\n')
+f.write(classification_report(y_test, predicted, target_names=corpus.target_names) + '\n')
+f.write('d) Accuracy, Macro Average F1, Weighted Average\n')
+f.write('accuracy: ' + str(accuracy_score(y_test, predicted)) + '\n')
+f.write('macro average f1: ' + str(f1_score(y_test, predicted, average='macro')) + '\n')
+f.write('weighted average: ' + str(f1_score(y_test, predicted, average='weighted')) + '\n')
+f.write('---------------------------------------------------------------------------------\n')
+f.write('e) Prior Probabilities\n')
+f.write('---------------------------------------------------------------------------------\n')
+f.write('f) Size of vocabulary\n')
+f.write('size of vocabulary ' + str(len((vectorizer.vocabulary_))))
+f.write('---------------------------------------------------------------------------------\n')
+
+
+
+
+
